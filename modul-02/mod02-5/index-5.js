369474aa69e4ed647768e4f8cd91f93ad36f21de
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2022-11-12 21:23:38 +0200

    mod02-5 tehty

---
+++ modul-02/mod02-5/index-5.js
@@ -0,0 +1,22 @@
+"use strict";
+let numbers = [];
+let n = parseInt(prompt("Anna numero: "));
+numbers.push(n);
+//let parent = document.querySelector("ul");
+let a = false;
+
+while (a == false) {
+  n = parseInt(prompt("Anna numero: "));
+  a = numbers.includes(n);
+  if (a == false) {
+    numbers.push(n);
+  }
+}
+
+numbers.sort((a, b) => a - b);
+
+for (let number of numbers) {
+  console.log(number);
+}
+
+//document.querySelector("#p1").innerHTML = a;
