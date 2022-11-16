d1113bb7c2e0a200b67ac73169c0206db5c65693
VladimirPin <vladimir.piniazhin@metropolia.fi>
2022-11-16 22:56:01 +0200

    03-9-1

--- modul-03/t9/9.js
+++ modul-03/t9/9.js
@@ -4,12 +4,14 @@ let p = document.querySelector("#result");
 const button = document.querySelector("#start");
 
 button.addEventListener("click", function() {
-    let string = document.querySelector("#calculation").value;
-    let list = string.split("");
-    let result = 
+    let string = +document.querySelector("#calculation").value;
+    console.log(string)
+    //let list = string.split("");
+    /*let result = 
     console.log(list);
     let n = list.length
     for (i = 0, i<=n, i++){
+        let result = 
         
-    }
+    }*/
 }); 
