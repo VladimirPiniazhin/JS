38305f58a3056bb82e15cccfd061188da8628742
VladimirPin <vladimir.piniazhin@metropolia.fi>
2022-11-16 20:54:16 +0200

    03-9 tehty

---
+++ modul-03/t9/9.js
@@ -0,0 +1,15 @@
+"use stric";
+
+let p = document.querySelector("#result");
+const button = document.querySelector("#start");
+
+button.addEventListener("click", function() {
+    let string = document.querySelector("#calculation").value;
+    let list = string.split("");
+    let result = 
+    console.log(list);
+    let n = list.length
+    for (i = 0, i<=n, i++){
+        
+    }
+}); 
