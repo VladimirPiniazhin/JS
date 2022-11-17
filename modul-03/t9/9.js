717912a3429dd87e51584bc99eb892a5e297479d
VladimirPin <vladimir.piniazhin@metropolia.fi>
2022-11-17 11:13:15 +0200

    03-9 tehty

--- modul-03/t9/9.js
+++ modul-03/t9/9.js
@@ -2,16 +2,56 @@
 
 let p = document.querySelector("#result");
 const button = document.querySelector("#start");
+/*
+function calculate(string) {
+    return (new Function('return ' + string))();
+  }
 
 button.addEventListener("click", function() {
-    let string = +document.querySelector("#calculation").value;
-    console.log(string)
-    //let list = string.split("");
-    /*let result = 
-    console.log(list);
-    let n = list.length
-    for (i = 0, i<=n, i++){
-        let result = 
-        
-    }*/
+    let string = document.querySelector("#calculation").value;
+    //console.log(calculate(string));
+    result = calculate(string)
+    p.innerHTML = "Result: "+result;
+    document.querySelector("#calculation").value = "";
 }); 
+*/
+button.addEventListener("click", function() {
+    let string = document.querySelector("#calculation").value;
+    let list1 = string.split("");
+    let list2 = string.split(/[+,-,/,*]/);
+    //result = +list.join();
+
+    //console.log(list1);
+    //console.log(list2);
+
+    for(let i=0; i<list1.length; i++) {
+        if(list1[i] == "+") {
+        let x1 = +list2[0];
+        let x2 = +list2[1];
+        let result = x1 + x2
+        console.log(result)
+        p.innerHTML = "Result: "+result;
+    } else if (list1[i] == "-") {
+        let x1 = +list2[0];
+        let x2 = +list2[1];
+        let result = x1 - x2
+        console.log(result)
+        p.innerHTML = "Result: "+result;
+    } else if (list1[i] == "/") {
+        let x1 = +list2[0];
+        let x2 = +list2[1];
+        let result = x1 / x2
+        console.log(result)
+        p.innerHTML = "Result: "+result;
+    } else if (list1[i] == "*") {
+        let x1 = +list2[0];
+        let x2 = +list2[1];
+        let result = x1 * x2
+        console.log(result)
+        p.innerHTML = "Result: "+result;
+}
+}
+document.querySelector("#calculation").value = "";
+});
+
+
