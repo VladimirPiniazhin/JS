9499a526d476ead32b4cf33df9459b1dae6cf2e0
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2022-11-17 13:26:47 +0200

    muutos 03-9

--- modul-03/t9/9.js
+++ modul-03/t9/9.js
@@ -2,43 +2,43 @@
 
 let p = document.querySelector("#result");
 const button = document.querySelector("#start");
-/*
+
 function calculate(string) {
-    return (new Function('return ' + string))();
+  return Function("return " + string)();
+}
+
+button.addEventListener("click", function () {
+  let string = document.querySelector("#calculation").value;
+  //console.log(calculate(string));
+  result = calculate(string);
+  //console.log(Function("return " + string));
+  p.innerHTML = "Result: " + result;
+  document.querySelector("#calculation").value = "";
+});
+
+/*button.addEventListener("click", function () {
+  let string = document.querySelector("#calculation").value;
+  let list1 = string.split("");
+  let list2 = string.split(/[+,\-,/,*]/);
+
+  let x1 = +list2[0];
+  let x2 = +list2[1];
+
+  console.log(list1);
+  console.log(list2);
+
+  if (string.includes("+") == true) {
+    let result = x1 + x2;
+    p.innerHTML = "Result: " + result;
+  } else if (string.includes("-") == true) {
+    let result = x1 - x2;
+    p.innerHTML = "Result: " + result;
+  } else if (string.includes("/") == true) {
+    let result = x1 / x2;
+    p.innerHTML = "Result: " + result;
+  } else if (string.includes("*") == true) {
+    let result = x1 * x2;
+    p.innerHTML = "Result: " + result;
   }
-
-button.addEventListener("click", function() {
-    let string = document.querySelector("#calculation").value;
-    //console.log(calculate(string));
-    result = calculate(string)
-    p.innerHTML = "Result: "+result;
-    document.querySelector("#calculation").value = "";
+  document.querySelector("#calculation").value = "";
 });*/
-
-button.addEventListener("click", function() {
-    let string = document.querySelector("#calculation").value;
-    let list1 = string.split("");
-    let list2 = string.split(/[+,\-,/,*]/);
-    
-    let x1 = +list2[0];
-    let x2 = +list2[1];
-
-    console.log(list1);
-    console.log(list2);
-    
-if(string.includes("+") == true) {
-    let result = x1 + x2
-    p.innerHTML = "Result: "+result;
-} else if (string.includes("\-") == true) {
-    let result = x1-x2
-    p.innerHTML = "Result: "+result;
-} else if (string.includes("/") == true) {
-    let result = x1 / x2
-    p.innerHTML = "Result: "+result;
-} else if (string.includes("*") == true) {
-    let result = x1 * x2
-    p.innerHTML = "Result: "+result;
-}
-document.querySelector("#calculation").value = "";
-}
-);
