debec3548082d822159e2c1fba1c8a6b5a205651
VladimirPin <vladimir.piniazhin@metropolia.fi>
2022-11-17 12:10:49 +0200

    03-9 fixed

--- modul-03/t9/9.js
+++ modul-03/t9/9.js
@@ -13,45 +13,32 @@ button.addEventListener("click", function() {
     result = calculate(string)
     p.innerHTML = "Result: "+result;
     document.querySelector("#calculation").value = "";
-}); 
-*/
+});*/
+
 button.addEventListener("click", function() {
     let string = document.querySelector("#calculation").value;
     let list1 = string.split("");
-    let list2 = string.split(/[+,-,/,*]/);
-    //result = +list.join();
+    let list2 = string.split(/[+,\-,/,*]/);
+    
+    let x1 = +list2[0];
+    let x2 = +list2[1];
 
-    //console.log(list1);
-    //console.log(list2);
-
-    for(let i=0; i<list1.length; i++) {
-        if(list1[i] == "+") {
-        let x1 = +list2[0];
-        let x2 = +list2[1];
-        let result = x1 + x2
-        console.log(result)
-        p.innerHTML = "Result: "+result;
-    } else if (list1[i] == "-") {
-        let x1 = +list2[0];
-        let x2 = +list2[1];
-        let result = x1 - x2
-        console.log(result)
-        p.innerHTML = "Result: "+result;
-    } else if (list1[i] == "/") {
-        let x1 = +list2[0];
-        let x2 = +list2[1];
-        let result = x1 / x2
-        console.log(result)
-        p.innerHTML = "Result: "+result;
-    } else if (list1[i] == "*") {
-        let x1 = +list2[0];
-        let x2 = +list2[1];
-        let result = x1 * x2
-        console.log(result)
-        p.innerHTML = "Result: "+result;
-}
+    console.log(list1);
+    console.log(list2);
+    
+if(string.includes("+") == true) {
+    let result = x1 + x2
+    p.innerHTML = "Result: "+result;
+} else if (string.includes("\-") == true) {
+    let result = x1-x2
+    p.innerHTML = "Result: "+result;
+} else if (string.includes("/") == true) {
+    let result = x1 / x2
+    p.innerHTML = "Result: "+result;
+} else if (string.includes("*") == true) {
+    let result = x1 * x2
+    p.innerHTML = "Result: "+result;
 }
 document.querySelector("#calculation").value = "";
-});
-
-
+}
+);
