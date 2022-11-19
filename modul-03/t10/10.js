4bde62923ed69d263d12ad68c177b50bc693b689
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2022-11-19 17:19:32 +0200

    10 tehty

---
+++ modul-03/t10/10.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let p = document.querySelector("#target");
+const button = document.querySelector("#submit");
+
+button.addEventListener("click", function () {
+  let num1 = +document.querySelector("#num1").value;
+  let num2 = +document.querySelector("#num2").value;
+  let select = document.querySelector("#operation");
+  let value = select.value;
+});
--- modul-03/t8/8.js
+++ modul-03/t8/8.js
@@ -1,31 +1,29 @@
-"use strict"
-
+"use strict";
 
 let p = document.querySelector("#result");
 const button = document.querySelector("#start");
 
-button.addEventListener("click", function() {
-    let num1 = +document.querySelector("#num1").value;
-    let num2 = +document.querySelector("#num2").value;
-    let select = document.querySelector("#operation");
-    let value = select.value;
+button.addEventListener("click", function () {
+  let num1 = +document.querySelector("#num1").value;
+  let num2 = +document.querySelector("#num2").value;
+  let select = document.querySelector("#operation");
+  let value = select.value;
 
-    //console.log(value);  
+  //console.log(value);
 
-    if (value === "add") {
-        let result = num1 + num2;
-        p.innerHTML = "Result: "+result;
-    } else if (value === "sub") {
-        let result = num1 - num2;
-        p.innerHTML = "Result: "+result;
-    } else if (value === "multi") {
-        let result = num1 * num2;
-        p.innerHTML = "Result: "+result;
-    } else if (value === "div") {
-        let result = num1 / num2;
-        p.innerHTML = "Result: "+result;
-    }
-    document.querySelector("#num1").value = "";
-    document.querySelector("#num2").value = "";
-    }); 
-    
+  if (value === "add") {
+    let result = num1 + num2;
+    p.innerHTML = "Result: " + result;
+  } else if (value === "sub") {
+    let result = num1 - num2;
+    p.innerHTML = "Result: " + result;
+  } else if (value === "multi") {
+    let result = num1 * num2;
+    p.innerHTML = "Result: " + result;
+  } else if (value === "div") {
+    let result = num1 / num2;
+    p.innerHTML = "Result: " + result;
+  }
+  document.querySelector("#num1").value = "";
+  document.querySelector("#num2").value = "";
+});
