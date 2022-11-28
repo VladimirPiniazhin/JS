e63b9a3405a5fa47a8364d33824352f5b707d259
VladimirPin <vladimir.piniazhin@metropolia.fi>
2022-11-28 22:55:08 +0200

    base 4 made

--- modul-04/index-4.js
+++ modul-04/index-4.js
@@ -1,40 +1,70 @@
-async function asynchronousFunction(get) {
-  // asynchronous function is defined by the async keyword
-  console.log("asynchronous download begins");
-  try {
-    // error handling: try/catch/finally
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q=${get}`
-    ); // starting data download, fetch returns a promise which contains an object of type 'response'
-    const jsonData = await response.json(); // retrieving the data retrieved from the response object using the json() function
-    console.log(jsonData); // log the result to the console
-  } catch (error) {
-    console.log(error.message);
-  } finally {
-    // finally = this is executed anyway, whether the execution was successful or not
-    console.log("asynchronous load complete");
+"use strict"
+
+async function getData(request) {
+  const response = await fetch('https://api.tvmaze.com/search/shows?q=' + request);
+  console.log('response', response);
+  const gotedData = await response.json();
+  //console.log('data', data)
+  return gotedData;
+}
+
+function renderHtml(data) {
+  const targetElem = document.querySelector('#search-result');
+  targetElem.innerHTML = "";
+  if (data.Error) {
+    console.log(data.Error);
+    targetElem.innerText = data.Error;
+  } else {
+    for (let obj of data) {
+      let content = `<article class="card">
+                      <h2>${obj["show"]["name"]}</h2>
+                      <figure>
+                        <img src="${(obj["show"]["image"]==null ? "No img.jpg":obj["show"]["image"]["medium"])}" alt="">
+                        <figcaption>${obj["show"]["genres"][0]}</figcaption>
+                     </figure>
+                     <a>${obj["show"]["url"]}</a></article>`;
+      targetElem.innerHTML += content;
+    }
+    
   }
 }
 
-//console.log("the script ends");
-let p = document.querySelector("#query").value;
+function handleFormSubmit(event) {
+  event.preventDefault()
+  serialForm(form)  
+} 
 
-function serialForm(formNode) {
+
+async function main() {
+  //const userInput = prompt('What are you looking for?');
+  //let form = document.forms[0].name; // <form name="my"> element
+
+  // получаем элемент
+  //let userInput = document.forms[0].name; // <input name="one"> element
+  //console.log(userInput)
+  //const userInput = document.getElementById('search-form')
+  //userInput.addEventListener("submit", handleFormSubmit)
+
+}
+
+async function serialForm(formNode) {
   const { elements } = formNode;
   const data = Array.from(elements)
     .filter((item) => !!item.name)
     .map((element) => {
-      const { name } = element;
-
-      return { name };
+      const { name, value } = element;
+      return { name, value };
     });
-}
+    let userInput = data[0]["value"]    
+    const filmData = await getData(userInput);
+    console.log('Data:', filmData);
+    renderHtml(filmData);
+  }
 
-function handleFormSubmit(event) {
-  // Älä lähetä lomaketta itse
-  event.preventDefault();
-  serialForm(asynchronousFunction(p));
-}
 
-const form = document.getElementById("source");
+
+const form = document.getElementById("search-form");
 form.addEventListener("submit", handleFormSubmit);
+
+
+console.log('ohjelma jatkuu');
